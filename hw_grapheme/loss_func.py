4bdd24a6a98010c78513cc5a6113240e1ac269e0
Nok Chan <channo@oocl.com>
2020-02-09 23:16:26 +0800

    add label_smoothing

--- hw_grapheme/loss_func.py
+++ hw_grapheme/loss_func.py
@@ -18,6 +18,60 @@ def mixup_criterion(preds1, preds2, preds3, targets):
     criterion = nn.CrossEntropyLoss(reduction='mean')
     return lam * criterion(preds1, targets1) + (1 - lam) * criterion(preds1, targets2) + lam * criterion(preds2, targets3) + (1 - lam) * criterion(preds2, targets4) + lam * criterion(preds3, targets5) + (1 - lam) * criterion(preds3, targets6)
 
+def lin_comb(a, b, t):
+    return t*a + (1-t) * b
+
+def reduce_loss(loss, reduction='mean'):
+    return loss.mean() if reduction == 'mean' else loss.sum() if reduction == 'sum' else loss
+
+
+#export
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, ε: float = 0.1, reduction='mean'):
+        super().__init__()
+        self.ε, self.reduction = ε, reduction
+
+    def forward(self, output, target):
+        c = output.size()[-1]
+        log_preds = F.log_softmax(output, dim=-1)
+        loss = reduce_loss(-log_preds.sum(dim=-1), self.reduction)
+        nll = F.nll_loss(log_preds, target, reduction=self.reduction)
+        return lin_comb(loss/c, nll, self.ε)
+
+class CombineLabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, ε: float = 0.1, reduction='mean'):
+        super().__init__()
+        self.ε, self.reduction = ε, reduction
+
+
+    def forward(self, input, root_target, vowel_target, consonant_target):
+        root_pred, vowel_pred, consonant_pred = input
+        root_pred = root_pred.float()
+        vowel_pred = vowel_pred.float()
+        consonant_pred = consonant_pred.float()
+
+        c_root = root_pred.size()[-1]
+        c_vowel = vowel_pred.size()[-1]
+        c_consonant = consonant_pred.size()[-1]
+
+        log_preds_root = F.log_softmax(root_pred, dim=-1)
+        log_preds_vowel = F.log_softmax(vowel_pred, dim=-1)
+        log_preds_consonant = F.log_softmax(consonant_pred, dim=-1)
+
+        loss_root = reduce_loss(-log_preds_root.sum(dim=-1), self.reduction)
+        loss_voewel = reduce_loss(-log_preds_vowel.sum(dim=-1), self.reduction)
+        loss_consonant = reduce_loss(
+            -log_preds_consonant.sum(dim=-1), self.reduction)
+
+        nll_root = F.nll_loss(log_preds_root, root_target,
+                              reduction=self.reduction)
+        nll_vowel = F.nll_loss(log_preds_vowel, vowel_target,
+                               reduction=self.reduction)
+        nll_consonant = F.nll_loss(
+            log_preds_consonant, consonant_target, reduction=self.reduction)
+        return 0.5 * lin_comb(loss_root/c_root, nll, self.ε) +\
+            0.25 * lin_comb(loss_voewel/c_vowel, nll, self.ε) +\
+            0.25 * lin_comb(loss_consonant/c_consonat, nll, self.ε)
 
 class Loss_combine(nn.Module):
     def __init__(self):
